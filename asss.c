f7c6cf949a1835092218df8cbed5bb245697877b
ajvaghasiya69 <138271801+ajvaghasiya69@users.noreply.github.com>
2023-07-10 16:03:11 +0530

    Add files via upload

---
+++ asss.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	
+	int a,b;
+	printf("Enter first number:\n");
+	scanf("%d",&a);
+	printf("Enter secound number:\n");
+	scanf("%d",&b);
+	
+     printf("Addition of %d\n",a+b);
+     printf("Substraction of %d\n",a-b);
+     printf("Multiplication of %d\n",a*b);
+     printf("Division of %d\n",a/b);
+     printf("Modulo of %d\n",a%b);
+}
---
+++ assss.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	float a=4;
+	printf("your Circle Area\n");
+	printf("area is:%f\n",3.14*a*a);
+	
+    int l=4,b=6;
+	printf("Your Rectangle Area\n");
+	printf("area is :%d\n",l*b);
+	
+	float c=4,h=5;
+	printf("Your Triangal Area\n");
+	printf("area is: %f",0.5*c*h);
+}

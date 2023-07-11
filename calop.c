dc4a0078f7e671531a8ce497617ed2abd1e74e01
ajvaghasiya69 <138271801+ajvaghasiya69@users.noreply.github.com>
2023-07-11 07:21:15 +0530

    Add files via upload

---
+++ calop.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+
+    printf("Enter second number:");
+    scanf("%d",&num2);
+    
+    printf("Enter operator (+, -, *, /, %):");
+    scanf(" %c",&operator);
+    
+    switch(operator)
+    {
+    	case'+':
+    		printf("%d + %d =%d",num1,num2,num1+num2);
+    		break;
+	
+	    case'-':
+    		printf("%d - %d =%d",num1,num2,num1-num2);
+    		break;
+    	case'*':
+    		printf("%d * %d =%d",num1,num2,num1*num2);
+    		break;
+    	case'+/':
+    		printf("%d / %d =%d",num1,num2,num1/num2);
+    		break;
+    	case'%':
+    		printf("%d %% %d =%d",num1,num2,num1%num2);
+    		break;
+    	default:
+            printf("Error: Invalid operator.\n");
+            break;
+        }
+
+}

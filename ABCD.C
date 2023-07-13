4e0c4ffdb5009d2175654e7e216204ab99a752dc
ajvaghasiya69 <138271801+ajvaghasiya69@users.noreply.github.com>
2023-07-13 15:33:34 +0530

    Add files via upload

---
+++ ABCD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	
+	printf("A\n");
+	printf("A B\n");
+	printf("A B C\n");
+	printf("A B C D\n");
+	printf("A B C D E\n");
+	printf("A B C D E F\n");
+	printf("A B C D E F G\n");
+}

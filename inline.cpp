114710e9f82e3fa82d778fc0fbe9e1dff96936bc
ajvaghasiya69 <138271801+ajvaghasiya69@users.noreply.github.com>
2023-07-14 16:12:49 +0530

    Add files via upload

--- inline.cpp
+++ inline.cpp
@@ -1,13 +1,25 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-inline int square(int a)
-       
-{
-	cout<<"square is "<<a*a;
+
+inline int multiply(int a, int b) {
+    return a * b;
 }
-int main()
-{
-	square(10);
+
+
+inline int cube(int num) {
+    return num * num * num;
+}
+
+int main() {
+    int num1, num2;
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+
+    int result_multiply = multiply(num1, num2);
+    int result_cube = cube(num1);
+
+    std::cout << "Multiplication: " << result_multiply << std::endl;
+    std::cout << "Cubic Value: " << result_cube << std::endl;
 
 }
+

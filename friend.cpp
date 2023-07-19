283e333c47756fbd4328a37b696874e1c42e3915
ajvaghasiya69 <138271801+ajvaghasiya69@users.noreply.github.com>
2023-07-19 07:23:39 +0530

    Add files via upload

---
+++ friend.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class money
+{
+	friend void dost();
+	public:
+		
+	int a=10;	
+	
+	protected :
+	int b=20;
+	
+	private :
+	int c=30;
+};
+ void dost()
+ {
+ 	money m;
+ 	cout<<m.a<<endl;
+ 		cout<<m.b<<endl;
+ 			cout<<m.c<<endl;
+ }
+ int main()
+ {
+ 	dost();
+ }
